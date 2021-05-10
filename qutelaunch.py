b7f9fe1789936e415b53a954581429f3741d193a
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-05-10 22:06:50 +0300

    Add a class for querying the browsing history.

---
+++ qutelaunch.py
@@ -0,0 +1,20 @@
+import sqlite3
+from collections import Counter
+from os import getenv
+from urllib.parse import urlparse
+
+
+class WebHistory:
+    def __init__(self, history_db_path: str = None):
+        if not history_db_path:
+            data_dir = getenv("XDG_DATA_HOME", getenv("HOME") + "/.local/share")
+            history_db_path = data_dir + "/qutebrowser/history.sqlite"
+        self._db = sqlite3.connect(history_db_path).cursor()
+
+    @property
+    def most_common_domains(self):
+        query = "SELECT url FROM history"
+        urls = (row[0] for row in self._db.execute(query))
+        domains = (urlparse(url).netloc for url in urls)
+        domains_counter = Counter(domains)
+        return domains_counter
