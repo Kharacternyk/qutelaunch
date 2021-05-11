c15581be2d83ec3d0e4b92f0e773c1d6c68d4e41
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-05-11 19:13:06 +0300

    Add a class for rendering Jinja templates.

--- qutelaunch.py
+++ qutelaunch.py
@@ -3,6 +3,8 @@ from collections import Counter
 from os import getenv
 from urllib.parse import urlparse
 
+import jinja2
+
 
 class WebHistory:
     def __init__(self, history_db_path: str = None):
@@ -19,3 +21,15 @@ class WebHistory:
         domains = (urlparse(url).netloc for url in urls)
         domains_counter = Counter(domains)
         return domains_counter
+
+
+class Renderer:
+    def __init__(self, templates_dir_path: str = "."):
+        self._jinja = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(templates_dir_path)
+        )
+
+    def render(self, template_file_name, **kwargs):
+        template = self._jinja.get_template(template_file_name)
+        result = template.render(**kwargs)
+        return result
