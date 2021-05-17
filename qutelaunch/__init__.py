b8e3b51181d3767d2e26e95393f03e594cbd3716
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-05-17 19:55:59 +0300

    Accept the length of the list as a parameter.

--- qutelaunch/__init__.py
+++ qutelaunch/__init__.py
@@ -5,10 +5,10 @@ from .web_history import WebHistory
 __all__ = ["init"]
 
 
-def init(config, c):
+def init(config, c, list_length=20):
     renderer = Renderer()
     web_history = WebHistory(config.datadir / "history.sqlite")
-    relevant_urls = get_relevant_urls(web_history.url_strings, 20)
+    relevant_urls = get_relevant_urls(web_history.url_strings, list_length)
     startpage = renderer.render("qutelaunch.html", relevant_urls=relevant_urls)
 
     with open(config.datadir / "qutelaunch.html", "w") as f:
