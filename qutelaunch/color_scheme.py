66135ec5a378f57f7a30e01754b18a45f87ffa58
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-05-21 21:13:30 +0300

    Alternate the background of odd and even rows.

--- qutelaunch/color_scheme.py
+++ qutelaunch/color_scheme.py
@@ -6,3 +6,5 @@ class ColorScheme:
     background: str = "#282828"
     foreground: str = "#fbf1c7"
     section_header: str = "#928374"
+    url_background_odd: str = "#3c3836"
+    url_background_even: str = "#504945"
