60453e5b8da2ea3ac4640d16e4ed74417a8f534b
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-05-29 16:39:49 +0300

    Extract write_target() into its own submodule.

--- qutelaunch/__init__.py
+++ qutelaunch/__init__.py
@@ -1,12 +1,8 @@
-import re
 from os import getenv
 from time import time
-from urllib.parse import urlparse
 
-from .bookmarks import Bookmarks
 from .color_scheme import ColorScheme
-from .renderer import Renderer
-from .web_history import WebHistory
+from .write_target import write_target
 
 __all__ = ["init", "ColorScheme"]
 
@@ -41,41 +37,3 @@ def init(
             exclude_patterns,
             recent_timespan,
         )
-
-
-def write_target(
-    target,
-    history_path,
-    bookmarks_path,
-    list_length,
-    color_scheme,
-    exclude_patterns,
-    recent_timespan,
-):
-    renderer = Renderer()
-    web_history = WebHistory(history_path)
-    bookmarks = Bookmarks(bookmarks_path)
-
-    exclude_regexes = re.compile(re.escape("file://" + str(target))), *(
-        re.compile(pattern) for pattern in exclude_patterns
-    )
-    most_visited_urls = web_history.get_most_visited_urls(
-        list_length, exclude_regexes=exclude_regexes
-    )
-    recent_urls = web_history.get_most_visited_urls(
-        list_length,
-        exclude_regexes=exclude_regexes,
-        since=time() - recent_timespan,
-    )
-
-    startpage = renderer.render(
-        "qutelaunch.html",
-        most_visited_urls=most_visited_urls,
-        recent_urls=recent_urls,
-        bookmarks_urls=bookmarks.urls,
-        color_scheme=color_scheme,
-        urlparse=urlparse,
-    )
-
-    with open(target, "w") as f:
-        print(startpage, file=f)
