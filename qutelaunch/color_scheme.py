d260e823bd18fd0e75a0ae7a4678d8f08cebf7c9
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-05-29 17:08:39 +0300

    Restructure the ColorScheme class.

--- qutelaunch/color_scheme.py
+++ qutelaunch/color_scheme.py
@@ -3,8 +3,11 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class ColorScheme:
-    background: str = "#282828"
-    foreground: str = "#fbf1c7"
-    section_header: str = "#928374"
-    url_background_odd: str = "#3c3836"
-    url_background_even: str = "#504945"
+    page_bg: str = "#282828"
+    header_fg: str = "#928374"
+    url_odd_bg: str = "#3c3836"
+    url_odd_fg: str = "#fbf1c7"
+    url_even_bg: str = "#504945"
+    url_even_fg: str = "#fbf1c7"
+    url_focused_bg: str = "#fbf1c7"
+    url_focused_fg: str = "#282828"
