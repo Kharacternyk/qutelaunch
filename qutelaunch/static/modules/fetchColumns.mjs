405ba40f89c696746c025d338db5293c71829a3c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-06-07 13:10:44 +0300

    Reset focus on a change of the query properly.

--- qutelaunch/static/modules/fetchColumns.mjs
+++ qutelaunch/static/modules/fetchColumns.mjs
@@ -1,19 +1,31 @@
+const columnNames = ["recent", "most-visited", "bookmarks"];
+
 export default function fetchColumns(query) {
-    const columnNames = ["recent", "most-visited", "bookmarks"];
     if (query === undefined || query === "") {
         query = "*";
     } else {
         query = "*" + query + "*";
     }
+    let promises = [];
     for (const columnName of columnNames) {
         const target = document.getElementById(columnName);
-        fetch(columnName + ".html?query=" + query)
+        promises.push(fetch(columnName + ".html?query=" + query)
             .then(response => response.text())
             .then(text => {
                 target.innerHTML = text;
-                if (columnName == "recent") {
-                    target.children[1].focus();
-                }
-            });
+                resetFocus();
+            })
+        );
+    }
+    Promise.all(promises).then(_ => resetFocus());
+}
+
+function resetFocus() {
+    for (const columnName of columnNames) {
+        const column = document.getElementById(columnName);
+        if (column.children.length > 1) {
+            column.children[1].focus();
+            break;
+        }
     }
 }
